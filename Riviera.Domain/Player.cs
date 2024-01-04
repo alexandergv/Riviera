eb6339b034128ae21838932d240eee0d425a8480
alexandergv <69660314+alexandergv@users.noreply.github.com>
2024-01-04 09:25:36 -0400

    Add project files.

---
+++ Riviera.Domain/Player.cs
@@ -0,0 +1,9 @@
+﻿namespace Riviera.Domain
+{
+    public static class Player
+    {
+        public static int HP { get; set; }
+        public static string Name { get; set; } = "Eduardo";
+
+    }
+}
